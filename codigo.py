2a8a8527c9c28ce194ad11f6ebf98b21c936330c
Daniel02-cmd <jesus-daniel2002@hotmail.com>
2025-01-30 14:57:22 -0500

    Creación histograma

--- codigo.py
+++ codigo.py
@@ -9,3 +9,13 @@ vals.shape
 
 vals1 = np.random.chisquare(df=3)
 vals1.shape
+
+import matplotlib.pyplot as plt
+count, bins, ignored = plt.hist(x=vals1, bins=30)
+plt.title('Histograma de valores generados')
+plt.xlabel('Tiempos de servicio')
+plt.ylabel('Frecuencia')
+plt.show()
+
+print(bins)
+print(count)
